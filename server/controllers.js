b2943e5c20c5f353414491d0cf86664ee7ebce38
KirkNotCaptain <cmariekirk@gmail.com>
2021-03-20 16:31:57 -0400

    Adjusted queries to restructure data to more closely match API call

--- server/controllers.js
+++ server/controllers.js
@@ -2,26 +2,28 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 let db;
 let productsCollection;
-let styleCollection;
+let stylesCollection;
+let skuCollection;
 
 MongoClient.connect(url, { useUnifiedTopology: true }) //
   .then((client) => {
     db = client.db('productsAPI');
     productsCollection = db.collection('products');
-    styleCollection = db.collection('styles');
+    stylesCollection = db.collection('styles');
+    skuCollection = db.collection('skus');
   })
   .catch((err) => {
     console.error(err);
   });
 
 /*=========================================================================
-  =                         Product Information                      =
-     Returns all product level information for a specified product id.
+  =                         List Product                            =
+                  Retrieves the list of products
   =========================================================================*/
-var queryProducts = (req, res) => {
-  var page = Number(req.query.page) || 1;
-  var count = Number(req.query.count) || 5;
-  var limit = page * count;
+let queryProducts = (req, res) => {
+  let page = Number(req.query.page) || 1;
+  let count = Number(req.query.count) || 5;
+  let limit = page * count;
 
   productsCollection
     .find({})
@@ -29,7 +31,25 @@ var queryProducts = (req, res) => {
     .limit(limit)
     .toArray()
     .then((docs) => {
-      res.status(200).send(docs);
+      if (!docs) {
+        res.status(200).send({});
+        return;
+      }
+      let response = [];
+
+      docs.forEach((res) => {
+        response.push({
+          id: res['id'],
+          name: res['name'],
+          slogan: res['slogan'],
+          description: res['description'],
+          category: res['category'],
+          default_price: res['default_price'].trim(),
+        });
+      });
+      //console.log(docs);
+
+      res.status(200).send(response);
     });
 };
 
@@ -37,10 +57,23 @@ var queryProducts = (req, res) => {
   =                         Product Information                      =
      Returns all product level information for a specified product id.
   =========================================================================*/
-var queryProductId = (req, res) => {
+let queryProductId = (req, res) => {
   let id = Number(req.params.product_id);
   productsCollection.findOne({ id }).then((docs) => {
-    res.status(200).send(docs);
+    if (!docs) {
+      res.status(200).send({});
+      return;
+    }
+    let response = {
+      id: docs['id'],
+      name: docs['name'],
+      slogan: docs['slogan'],
+      description: docs['description'],
+      category: docs['category'],
+      default_price: docs['default_price'].trim(),
+      features: docs['features'],
+    };
+    res.status(200).send(response);
   });
 };
 
@@ -49,11 +82,63 @@ var queryProductId = (req, res) => {
       Returns the id's of products related to the product specified.
   =========================================================================*/
 
-var queryRelatedProducts = (req, res) => {
+let queryRelatedProducts = (req, res) => {
   let id = Number(req.params.product_id);
   productsCollection.findOne({ id }).then((docs) => {
-    var relatedProducts = docs['related'];
+    if (!docs) {
+      res.status(200).send({});
+      return;
+    }
+    let relatedProducts = docs['related'] || [];
     res.status(200).send(relatedProducts);
+  });
+};
+
+/*=========================================================================
+=                         Product Styles                        =
+Returns the all styles available for the given product.
+=========================================================================*/
+let queryProductStyles = (req, res) => {
+  console.log(req.params);
+  let product_id = Number(req.params.product_id);
+  let response = {};
+
+  return new Promise((resolve, reject) => {
+    stylesCollection.findOne({ product_id }).then((styleDocs) => {
+      if (!styleDocs) {
+        res.status(200).send({});
+        return;
+      }
+      response = {
+        product_id: styleDocs['product_id'],
+        results: styleDocs['results'],
+      };
+      resolve(response);
+    });
+  }).then((response) => {
+    skuCollection.findOne({ _id: product_id }).then((skuDocs) => {
+      if (!skuDocs) {
+        res.status(200).send({});
+        return;
+      }
+      let skus = {};
+
+      skuDocs['skus'].forEach((sku) => {
+        skus[sku['id']] = {
+          size: sku['size'],
+          quantity: sku['quantity'],
+        };
+      });
+
+      response.results.forEach((result) => {
+        result['default?']
+          ? (result['default?'] = true)
+          : (result['default?'] = false);
+        result['skus'] = skus;
+      });
+
+      res.status(200).send(response);
+    });
   });
 };
 
@@ -61,4 +146,5 @@ module.exports = {
   queryProducts: queryProducts,
   queryProductId: queryProductId,
   queryRelatedProducts: queryRelatedProducts,
+  queryProductStyles: queryProductStyles,
 };
--- server/routes.js
+++ server/routes.js
@@ -4,8 +4,6 @@ const controllers = require('./controllers.js');
 router.get('/products', controllers.queryProducts);
 router.get('/products/:product_id', controllers.queryProductId);
 router.get('/products/:product_id/related', controllers.queryRelatedProducts);
+router.get('/products/:product_id/styles', controllers.queryProductStyles);
 
 module.exports = router;
-//TODO: router.get for products/:product_id
-//TODO: router.get for products/:product_id/styles
-//TODO: router.get for products/:product_id/related
