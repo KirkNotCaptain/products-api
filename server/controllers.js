1a803e97c202a678abdbcabbb9f0b8e194cf0b27
KirkNotCaptain <cmariekirk@gmail.com>
2021-03-22 14:32:43 -0400

    Modified ETL to restructure sku data to have the sku_id as the object key. Restructured the styles query function to use projection and to work with new data structure

--- server/controllers.js
+++ server/controllers.js
@@ -19,6 +19,9 @@ MongoClient.connect(url, { useUnifiedTopology: true }) //
 /*=========================================================================
   =                         List Product                            =
                   Retrieves the list of products
+                            Parameters:
+     page - int - Selects the page of results to return. Default 1.
+     count - int = Specifies how many results per page to return. Default 5.
   =========================================================================*/
 let queryProducts = (req, res) => {
   let page = Number(req.query.page) || 1;
@@ -36,7 +39,6 @@ let queryProducts = (req, res) => {
     .limit(limit)
     .toArray()
     .then((docs) => {
-      console.log(docs);
       if (!docs) {
         res.status(200).send({});
         return;
@@ -49,6 +51,8 @@ let queryProducts = (req, res) => {
 /*=========================================================================
   =                         Product Information                      =
      Returns all product level information for a specified product id.
+                              Parameters:
+          product_id - int - Required ID of the Product requested
   =========================================================================*/
 let queryProductId = (req, res) => {
   let id = Number(req.params.product_id);
@@ -66,6 +70,8 @@ let queryProductId = (req, res) => {
 /*=========================================================================
   =                         Related Products                      =
       Returns the id's of products related to the product specified.
+                            Parameters:
+        product_id - int - Required ID of the Product requested
   =========================================================================*/
 let queryRelatedProducts = (req, res) => {
   let id = Number(req.params.product_id);
@@ -84,51 +90,87 @@ let queryRelatedProducts = (req, res) => {
 /*=========================================================================
 =                         Product Styles                        =
       Returns the all styles available for the given product.
+                          Parameters:
+      product_id - int - Required ID of the Product requested
 =========================================================================*/
+// let queryProductStyles = (req, res) => {
+//   let product_id = Number(req.params.product_id);
+//   let response = {};
+
+//   return new Promise((resolve, reject) => {
+//     stylesCollection.findOne({ product_id }).then((styleDocs) => {
+//       if (!styleDocs) {
+//         res.status(200).send({});
+//         return;
+//       }
+//       response = {
+//         product_id: styleDocs['product_id'],
+//         results: styleDocs['results'],
+//       };
+//       resolve(response);
+//     });
+//   }).then((response) => {
+//     skuCollection.findOne({ _id: product_id }).then((skuDocs) => {
+//       if (!skuDocs) {
+//         res.status(200).send({});
+//         return;
+//       }
+//       let skus = {};
+
+//       skuDocs['skus'].forEach((sku) => {
+//         skus[sku['id']] = {
+//           size: sku['size'],
+//           quantity: sku['quantity'],
+//         };
+//       });
+
+//       response.results.forEach((result) => {
+//         if (result['sale_price'] === 'null') {
+//           result['sale_price'] = null;
+//         }
+//         result['default?']
+//           ? (result['default?'] = true)
+//           : (result['default?'] = false);
+//         result['skus'] = skus;
+//       });
+
+//       res.status(200).send(response);
+//     });
+//   });
+// };
+
 let queryProductStyles = (req, res) => {
-  console.log(req.params);
   let product_id = Number(req.params.product_id);
   let response = {};
 
   return new Promise((resolve, reject) => {
-    stylesCollection.findOne({ product_id }).then((styleDocs) => {
-      if (!styleDocs) {
-        res.status(200).send({});
-        return;
-      }
-      response = {
-        product_id: styleDocs['product_id'],
-        results: styleDocs['results'],
-      };
-      resolve(response);
-    });
-  }).then((response) => {
-    skuCollection.findOne({ _id: product_id }).then((skuDocs) => {
-      if (!skuDocs) {
-        res.status(200).send({});
-        return;
-      }
-      let skus = {};
-
-      skuDocs['skus'].forEach((sku) => {
-        skus[sku['id']] = {
-          size: sku['size'],
-          quantity: sku['quantity'],
-        };
-      });
-
-      response.results.forEach((result) => {
-        if (result['sale_price'] === 'null') {
-          result['sale_price'] = null;
+    stylesCollection
+      .findOne({ product_id }, { projection: { _id: false } })
+      .then((styleDocs) => {
+        if (!styleDocs) {
+          res.status(200).send({});
+          return;
         }
-        result['default?']
-          ? (result['default?'] = true)
-          : (result['default?'] = false);
-        result['skus'] = skus;
+        resolve(styleDocs);
       });
-
-      res.status(200).send(response);
-    });
+  }).then((styleDocs) => {
+    skuCollection
+      .findOne({ _id: product_id }, { projection: { _id: false } })
+      .then((skuDocs) => {
+        if (!skuDocs) {
+          res.status(200).send({});
+        }
+        styleDocs['results'].forEach((style) => {
+          if (style['sale_price'] === 'null') {
+            style['sale_price'] = null;
+          }
+          style['default?']
+            ? (style['default?'] = true)
+            : (style['default?'] = false);
+          style['skus'] = skuDocs.skus;
+        });
+        res.status(200).send(styleDocs);
+      });
   });
 };
 
