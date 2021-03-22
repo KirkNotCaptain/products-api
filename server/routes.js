3518a3728e75da3727df726a95420c3e9d1f96ca
KirkNotCaptain <cmariekirk@gmail.com>
2021-03-22 16:48:11 -0400

    Created dockerfile for server that successfully runs image in container

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const controllers = require('./controllers.js');
 
+router.get('/', (req, res) => {
+  res.send('Welcome to the products api');
+});
 router.get('/products', controllers.queryProducts);
 router.get('/products/:product_id', controllers.queryProductId);
 router.get('/products/:product_id/related', controllers.queryRelatedProducts);
