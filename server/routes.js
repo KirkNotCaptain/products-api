2c1dd885f5ef57ea37f1ab3ec0f3fdbef0dd4a09
Ubuntu <ubuntu@ip-172-31-32-59.us-east-2.compute.internal>
2021-03-25 16:13:30 +0000

    added loader.io route

--- server/routes.js
+++ server/routes.js
@@ -4,6 +4,9 @@ const controllers = require('./controllers.js');
 router.get('/', (req, res) => {
   res.send('welcome to the products api');
 });
+router.get('/loaderio-ebbc3b3c895f6ba370e446f98015309e', (req, res) => {
+  res.send('loaderio-ebbc3b3c895f6ba370e446f98015309e');
+});
 router.get('/products', controllers.queryProducts);
 router.get('/products/:product_id', controllers.queryProductId);
 router.get('/products/:product_id/related', controllers.queryRelatedProducts);
